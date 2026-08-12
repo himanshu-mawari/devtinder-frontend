b373e9f1946949afd8573b8d5be34c5bd654d99f
himanshu-mawari <mawrihimanshu83@gmail.com>
2026-08-12 18:04:13 +0530

    feat: implement rtk Query base api and store configuration

---
+++ src/services/baseApi.js
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+
+
+export const baseApi = createApi({
+  reducerPath: "baseApi",
+
+  baseQuery: fetchBaseQuery({
+    baseUrl:  import.meta.env.VITE_API_BASE_URL,
+    credentials: "include",
+  }),
+
+   tagTypes: ["User", "Feed", "Connection", "Request", "Message", "Chat"],
+  endpoints: () => ({}),
+});
+
+
