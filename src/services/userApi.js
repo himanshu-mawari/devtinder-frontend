064ddf513552cdb47d713ac30ff5e9dab9c41b8c
himanshu-mawari <mawrihimanshu83@gmail.com>
2026-08-20 23:29:57 +0530

    feat: integrate get other user detail api

--- src/services/userApi.js
+++ src/services/userApi.js
@@ -1,15 +1,21 @@
 import { baseApi } from "./baseApi";
 
 export const userApi = baseApi.injectEndpoints({
-    endpoints:(builder) => ({
-     getProfile : builder.query({
-        query:( ) => ({
-            url: "profile/view",
-            method: "GET"
-        }),
-        providesTags:["User"]
-     })
-    })
+  endpoints: (builder) => ({
+    getProfile: builder.query({
+      query: () => ({
+        url: "profile/view",
+        method: "GET",
+      }),
+      providesTags: ["User"],
+    }),
+    getUserDetail: builder.query({
+      query: ({userId}) => ({
+        url: `user/${userId}`,
+      }),
+      transformResponse:  (response) => response?.data
+    }),
+  }),
 });
 
-export const {useGetProfileQuery} = userApi;
+export const { useGetProfileQuery , useGetUserDetailQuery} = userApi;
