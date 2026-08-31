ea49f7d60f1610502d999081232422626241adec
himanshu-mawari <mawrihimanshu83@gmail.com>
2026-08-31 16:20:31 +0530

    feat: register ConnectionRequests tag on base api

--- src/services/baseApi.js
+++ src/services/baseApi.js
@@ -1,17 +1,21 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-
-
 export const baseApi = createApi({
   reducerPath: "baseApi",
 
   baseQuery: fetchBaseQuery({
-    baseUrl:  import.meta.env.VITE_API_BASE_URL,
+    baseUrl: import.meta.env.VITE_API_BASE_URL,
     credentials: "include",
   }),
 
-   tagTypes: ["User", "Feed", "Connection", "Request", "Message", "Chat"],
+  tagTypes: [
+    "User",
+    "Feed",
+    "Connection",
+    "Request",
+    "Message",
+    "Chat",
+    "ConnectionRequests",
+  ],
   endpoints: () => ({}),
 });
-
-
