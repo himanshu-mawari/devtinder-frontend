6a0a79437a340bd30169ac153ceca9fe5e746dc8
himanshu-mawari <mawrihimanshu83@gmail.com>
2026-09-01 15:06:27 +0530

    refactor: add id field to NAV_ITEMS and clean up formatting

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,10 @@
 import { Compass, Users, MessageSquare, User } from "lucide-react";
 
-
 export const BASE_URL = "http://localhost:2007/";
 
 export const NAV_ITEMS = [
-  { path: "/discover", label: "Discover", icon: Compass },
-  { path: "/connections", label: "Connections", icon: Users },
-  { path: "/dm", label: "Messages", icon: MessageSquare },
-  { path: "/profile", label: "Profile", icon: User ,  mobileOnly: true},
+  { id: 1, path: "/discover", label: "Discover", icon: Compass },
+  { id: 2, path: "/connections", label: "Connections", icon: Users },
+  { id: 3, path: "/dm", label: "Messages", icon: MessageSquare },
+  { id: 4, path: "/profile", label: "Profile", icon: User, mobileOnly: true },
 ];
