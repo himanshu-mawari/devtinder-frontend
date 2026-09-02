c74652ab0eb9a4ac09715ebddfc0e35dd34892a0
himanshu-mawari <mawrihimanshu83@gmail.com>
2026-09-02 18:50:07 +0530

    feat: migrate to infiniteQuery for page-based pagination

--- src/services/feedApi.js
+++ src/services/feedApi.js
@@ -2,15 +2,20 @@ import { baseApi } from "./baseApi";
 
 export const feedApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
-    feed: builder.query({
-      query: () => ({
-        url: "/user/feed",
-        method: "GET",
-      }),
-      providesTags:["Feed"]              ,
-      transformResponse: (response) => response?.data
+    feed: builder.infiniteQuery({
+      infiniteQueryOptions: {
+        initialPageParam: 1,
+        getNextPageParam: (lastPage,allPages, lastPageParam) => {
+          return lastPage?.pagination?.hasMore ? lastPageParam + 1 : undefined;
+        },
+      },
+
+      query({ pageParam }) {
+        console.log("pageParam value:", pageParam, typeof pageParam);
+        return `user/feed?page=${pageParam}`;
+      },
     }),
   }),
 });
 
-export const { useFeedQuery } = feedApi;
+export const { useFeedInfiniteQuery } = feedApi;
